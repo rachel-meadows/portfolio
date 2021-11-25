73c0914534311dc72963ff42c2eb4ff75c2144c5
rachel-meadows <rachelnewzealand@gmail.com>
2021-11-25 14:33:51 +1300

    Outline of new home page

--- README.md
+++
@@ -1,2 +0,0 @@
-# Project Site
-Site to host personal projects and CV
--- script.js
+++ script.js
@@ -1,34 +1,12 @@
-// Initialise items in skill list for DOM manipulation on hover
-const htmlSkills = document.getElementById("html");
-const reduxSkills = document.getElementById("Redux");
-const djangoSkills = document.getElementById("Django");
-const cssSkills = document.getElementById("css");
-const reactSkills = document.getElementById("react");
-const expressJSSkills = document.getElementById("expressJS");
-const javascriptSkills = document.getElementById("javaScript");
-const restfulAPIsSkills = document.getElementById("restfulAPIs");
-const nodeJSSkills = document.getElementById("nodeJS");
-const pythonSkills = document.getElementById("python");
-const sqlSkills = document.getElementById("sql");
-const handlebarsSkills = document.getElementById("handlebars");
-const teamManagementSkills = document.getElementById("teamManagement");
-const productOwnershipSkills = document.getElementById("productOwnership");
-const clientLiaisonSkills = document.getElementById("clientLiaison");
-const publicSpeakingSkills = document.getElementById("publicSpeaking");
-const consultingSkills = document.getElementById("consulting");
-const researchSkills = document.getElementById("research");
+
+const skills = document.querySelector("#skills");
+const subNavigation = document.querySelector("#subNavigation");
+const readMore = document.querySelector("#readMore");
 
 
-document.querySelectorAll('.skill').forEach(skill => {
-    skill.addEventListener('click', event => {
-        console.log("BBBBBBB");
-        skillText = document.getElementById(`${skill.id}Text`);
-        if (skillText.classList.contains("skillTextVisible")) {
-            skillText.classList.remove("skillTextVisible");
-        } else {
-            skillText.classList.add("skillTextVisible");
-            console.log("DDDDDDD");
-        }
-        });
+function scrollToContent() {
+    skills.scrollIntoView({behavior: "smooth"});
+}
+readMore.addEventListener("click", scrollToContent);
 
-})
+// https://webdesign.tutsplus.com/tutorials/how-to-intersection-observer--cms-30250
