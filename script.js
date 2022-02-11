6303517e149cf034130eea07136349eafa5feddc
Rachel Meadows <rachelnewzealand@gmail.com>
2022-02-12 10:54:49 +1300

    Make navigation initially hidden, appearing on scroll

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-
 const skills = document.querySelector("#skills");
-const subNavigation = document.querySelector("#subNavigation");
+const nav = document.querySelector(".navigation");
 const readMore = document.querySelector("#readMore");
+const header = document.querySelector("#aboveTheFold");
 
-
+// Index.html
 function scrollToContent() {
     skills.scrollIntoView({behavior: "smooth"});
 }
 readMore.addEventListener("click", scrollToContent);
 
-// https://webdesign.tutsplus.com/tutorials/how-to-intersection-observer--cms-30250
+// All pages
+window.onscroll = function() {
+    makeNavSticky()
+};
+
+function makeNavSticky() {
+    let headerHeight = header.offsetHeight;
+  if (window.pageYOffset > (headerHeight - 10)) {
+    nav.classList.add("sticky");
+  } else {
+    nav.classList.remove("sticky");
+  }
+} 
