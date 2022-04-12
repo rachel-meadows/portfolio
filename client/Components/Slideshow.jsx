15b1e5da7ea950a995e5cba3d847a38bc44300c6
Rachel Meadows <rachelnewzealand@gmail.com>
2022-04-12 20:54:19 +1200

    Complete no-video page slider functionality, resolving #20

--- client/Components/Slideshow.jsx
+++ client/Components/Slideshow.jsx
@@ -10,8 +10,35 @@ function Slideshow( { data } ) {
   function updateIndex(itemIndex) {
     setIndex(itemIndex)
   }
+
+  function decreaseIndex() {
+    if (index === 0) {
+      // If index is already at the start, go to the end
+      setIndex(images.length - 1)
+    } else {
+      setIndex(index - 1)
+    }
+  }
+  
+  function increaseIndex() {
+    if (index === images.length - 1) {
+      // If index is already at the end, go to the start
+      setIndex(0)
+    } else {
+      setIndex(index + 1)
+    }
+  }
+
   return (
     <div className="slideshow">
+
+      <a className="prev" onClick={() => {
+            decreaseIndex();
+          }}>&#10094;</a>
+      <a className="next" onClick={() => {
+            increaseIndex();
+          }}>&#10095;</a>
+
       <div className="slideshowSlider" style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}>
         {images.map((image, index) => (
           <div className="slide" key={index} style={{ backgroundImage: `url(${image})` }}/>
@@ -20,7 +47,6 @@ function Slideshow( { data } ) {
 
       <div className="slideshowDots">
         {images.map((item, itemIndex) => (
-          
           <div className={`slideshowDot${itemIndex === index ? " active" : ""}`} 
           key={itemIndex}
           onClick={() => {
@@ -28,6 +54,7 @@ function Slideshow( { data } ) {
           }}></div>
         ))}
       </div>
+
     </div>
   );
 }
